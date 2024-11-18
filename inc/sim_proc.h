ca656d0a5daa02f87338d4b976d7f954b3bb41ef
ManasVB <mbuzruk09@gmail.com>
2024-11-18 16:07:47 -0500

    set data structures

--- inc/sim_proc.h
+++ inc/sim_proc.h
@@ -1,12 +1,37 @@
 #ifndef SIM_PROC_H
 #define SIM_PROC_H
 
+#include <cstdint>
+#include <map>
+
+#pragma pack(1)
 typedef struct proc_params{
     unsigned long int rob_size;
     unsigned long int iq_size;
     unsigned long int width;
 }proc_params;
 
-// Put additional data structures here as per your requirement
+typedef struct {
+    uint8_t dst;    // R0-R66
+    uint32_t pc;
+    bool rdy;
+} ROB;
+
+typedef struct {
+    bool valid;
+    uint32_t dst_tag;
+
+    bool rs1_rdy;
+    uint32_t rs1_tag;
+
+    bool rs2_rdy;
+    uint32_t rs2_tag;
+} IQ;
+
+typedef struct {
+    bool valid;
+    uint32_t ROB_tag;
+} RMT;
+
 
 #endif
